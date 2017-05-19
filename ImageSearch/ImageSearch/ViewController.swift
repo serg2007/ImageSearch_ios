fbde611cf751674a48f5e898021422df0ff46e08
Sergij Sobol <1sergij2007@gmail.com>
2017-05-19 20:50:43 +0300

    init

---
+++ ImageSearch/ImageSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageSearch
+//
+//  Created by Sergiy Sobol on 19.05.17.
+//  Copyright © 2017 Sergiy Sobol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
